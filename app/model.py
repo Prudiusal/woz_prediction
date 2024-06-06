ffb9ae1667342a715b999cf2804af5c1a207f1c7
Aleksei <aleksei.vitalic@gmail.com>
2024-06-06 18:28:56 +0200

    with linters

--- app/model.py
+++ app/model.py
@@ -1,31 +1,23 @@
 import xgboost as xgb
 
+
 def load_model(model_path):
     """
-    The function `load_model` loads an XGBoost regressor model from a specified file path.
-    
-    :param model_path: The `model_path` parameter in the `load_model` function is the file path where
-    the XGBoost model is saved. This function loads the XGBoost model from the specified file path using
-    the `load_model` method of the `XGBRegressor` class
-    :return: The function `load_model` returns an XGBoost regressor model loaded from the specified
+    :param model_path: path of the model
+    :return: returns an XGBoost regressor model loaded from the specified
     model path.
     """
     model = xgb.XGBRegressor()
     model.load_model(model_path)
     return model
 
+
 def predict(model, data):
     """
-    The `predict` function takes a model and data as input, predicts the outcome using the model, and
-    returns the prediction.
-    
-    :param model: A machine learning model that has been trained on a dataset and is capable of making
-    predictions on new data
-    :param data: It seems like you were about to provide some information about the `data` parameter,
-    but it got cut off. Could you please provide more details or specific examples of what the `data`
-    parameter might contain? This will help me assist you better with the `predict` function
-    :return: The function `predict` returns the prediction made by the model for the input data
-    provided.
+
+    :param model: A machine learning model that has been trained on a dataset
+    :param data: request data in the form of a dictionary
+    :return: The function `predict` returns the numerical prediction.
     """
     prediction = model.predict([list(data.values())])
     return prediction[0]
--- tests/test_app.py
+++ tests/test_app.py
@@ -10,106 +10,127 @@ import xgboost
 
 load_dotenv()
 
+
 def test_model_path():
-    model_path = os.getenv('MODEL_PATH')
-    assert Path(model_path).exists() 
+    model_path = os.getenv("MODEL_PATH")
+    assert Path(model_path).exists()
+
 
 def test_load_model():
-    model_path = os.getenv('MODEL_PATH')
+    model_path = os.getenv("MODEL_PATH")
     model = load_model(model_path)
     assert isinstance(model, xgboost.sklearn.XGBRegressor)
 
+
 def test_prediction_model():
-    model_path = os.getenv('MODEL_PATH')
+    model_path = os.getenv("MODEL_PATH")
     model = load_model(model_path)
     data = {
-        'single': 10,
-        'married_no_kids': 5,
-        'not_married_no_kids': 2,
-        'married_with_kids': 8,
-        'not_married_with_kids': 3,
-        'single_parent': 1,
-        'other': 0,
-        'total': 29
+        "single": 10,
+        "married_no_kids": 5,
+        "not_married_no_kids": 2,
+        "married_with_kids": 8,
+        "not_married_with_kids": 3,
+        "single_parent": 1,
+        "other": 0,
+        "total": 29,
     }
     prediction = model.predict([list(data.values())])[0]
-    print(f'{prediction=}')
-    assert math.isclose(prediction, 1219730.9, rel_tol=1e-2)   
+    print(f"{prediction=}")
+    assert math.isclose(prediction, 1219730.9, rel_tol=1e-2)
+
 
 def test_predict():
-    model_path = os.getenv('MODEL_PATH')
+    model_path = os.getenv("MODEL_PATH")
     model = load_model(model_path)
     data = {
-        'single': 10,
-        'married_no_kids': 5,
-        'not_married_no_kids': 2,
-        'married_with_kids': 8,
-        'not_married_with_kids': 3,
-        'single_parent': 1,
-        'other': 0,
-        'total': 29
+        "single": 10,
+        "married_no_kids": 5,
+        "not_married_no_kids": 2,
+        "married_with_kids": 8,
+        "not_married_with_kids": 3,
+        "single_parent": 1,
+        "other": 0,
+        "total": 29,
     }
     prediction = predict(model, data)
     assert math.isclose(prediction, 1219730.9, rel_tol=1e-2)
+
 
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
 
+
 def test_get_woz_value(client):
-    response = client.get('/api/get_woz_value', query_string={
-        'single': 10,
-        'married_no_kids': 5,
-        'not_married_no_kids': 2,
-        'married_with_kids': 8,
-        'not_married_with_kids': 3,
-        'single_parent': 1,
-        'other': 0,
-        'total': 29
-    })
+    response = client.get(
+        "/api/get_woz_value",
+        query_string={
+            "single": 10,
+            "married_no_kids": 5,
+            "not_married_no_kids": 2,
+            "married_with_kids": 8,
+            "not_married_with_kids": 3,
+            "single_parent": 1,
+            "other": 0,
+            "total": 29,
+        },
+    )
     assert response.status_code == 200
-    assert 'woz_value' in response.json
-    
+    assert "woz_value" in response.json
+
+
 def test_get_woz_prediction(client):
-    response = client.get('/api/get_woz_value', query_string={
-        'single': 10,
-        'married_no_kids': 5,
-        'not_married_no_kids': 2,
-        'married_with_kids': 8,
-        'not_married_with_kids': 3,
-        'single_parent': 1,
-        'other': 0,
-        'total': 29
-    })
+    response = client.get(
+        "/api/get_woz_value",
+        query_string={
+            "single": 10,
+            "married_no_kids": 5,
+            "not_married_no_kids": 2,
+            "married_with_kids": 8,
+            "not_married_with_kids": 3,
+            "single_parent": 1,
+            "other": 0,
+            "total": 29,
+        },
+    )
     assert response.status_code == 200
-    assert 'woz_value' in response.json
-    prediction = response.json['woz_value']
+    assert "woz_value" in response.json
+    prediction = response.json["woz_value"]
     assert math.isclose(float(prediction), 1219730.9, rel_tol=1e-2)
 
+
 def test_missing_parameter(client):
-    response = client.get('/api/get_woz_value', query_string={
-        'single': 10,
-        'married_no_kids': 5,
-        'not_married_no_kids': 2,
-        'married_with_kids': 8,
-        'not_married_with_kids': 3,
-        'single_parent': 1,
-        'other': 0
-    })  # 'total' is missing
+    response = client.get(
+        "/api/get_woz_value",
+        query_string={
+            "single": 10,
+            "married_no_kids": 5,
+            "not_married_no_kids": 2,
+            "married_with_kids": 8,
+            "not_married_with_kids": 3,
+            "single_parent": 1,
+            "other": 0,
+        },
+    )  # 'total' is missing
     assert response.status_code == 500
-    assert 'error' in response.json
+    assert "error" in response.json
+
 
 def test_invalid_parameter_format(client):
-    response = client.get('/api/get_woz_value', query_string={
-        'single': 'wrong_format',
-        'married_no_kids': 5,
-        'not_married_no_kids': 2,
-        'married_with_kids': 8,
-        'not_married_with_kids': 3,
-        'single_parent': 1,
-        'other': 0,
-        'total': 29
-    })
+    response = client.get(
+        "/api/get_woz_value",
+        query_string={
+            "single": "wrong_format",
+            "married_no_kids": 5,
+            "not_married_no_kids": 2,
+            "married_with_kids": 8,
+            "not_married_with_kids": 3,
+            "single_parent": 1,
+            "other": 0,
+            "total": 29,
+        },
+    )
     assert response.status_code == 500
-    assert 'error' in response.json
+    assert "error" in response.json
